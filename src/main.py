26f9885e6b27a1588abd9d6d208d6d5a3e363266
Gregory Presser <gpress2222@gmail.com>
2022-12-09 12:41:58 -0600

    fixed count

--- src/main.py
+++ src/main.py
@@ -17,6 +17,7 @@ def main():
 
     # Not seeding with abstract just becasue it should be in there we can add it back later
     questions = []
+    n_q = 0
     size = 0
     bar = tqdm(pars)
     for par in bar:
@@ -27,14 +28,15 @@ def main():
         sleep(5) # I hate rate limits
         if question.useful:
             size += len(" ".join(question.questions))
+            n_q += len(question.questions)
             questions.append((par, question))
-        bar.set_description(f"len = {size} / 2000: count {len(questions)}")
+        bar.set_description(f"len = {size} / 2000: count {len(n_q)}")
         bar.refresh()
 
     ranking = rank_prompt(questions).split("\n")
     for i,q in enumerate(ranking):
         if i == 0:
-            print(f"{i}. {q}")
+            print(f" 1. {q}")
             continue
         print(q)
 
